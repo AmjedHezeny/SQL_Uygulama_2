24bf8562b76bc6d0b589b0f21eb7c66f553087a0
EMCET HAZANİ <140171298+EMCET-HAZANi@users.noreply.github.com>
2023-09-17 15:26:38 +0300

    Update SQL_Sorgu_2.sql

--- SQL_Sorgu_2.sql
+++ SQL_Sorgu_2.sql
@@ -1,47 +1,47 @@
--- Stok miktar? 0 ile 250 aras?nda olan �r�nleri listeleyelim
+-- Stok miktarı 0 ile 250 arasında olan ürünleri listeleyelim
 -- (Products)(UnitsInStock)
 SELECT
-    ProductName AS '�r�n Ad?',
-    UnitsInStock AS 'Stok Miktar?'
+    ProductName AS 'Ürün Adı',
+    UnitsInStock AS 'Stok Miktarı'
 FROM Products
 WHERE	
     UnitsInStock BETWEEN 0 AND 250
 ORDER BY UnitsInStock ASC;
 
--- Kargo �creti 50$ ile 100$ aras?nda olan sipari?leri listeleyelim
+-- Kargo ücreti 50$ ile 100$ arasında olan siparişleri listeleyelim
 -- (Orders)(Freight)
 SELECT
-    OrderID AS 'Sipari? ID',
-    CustomerID AS 'M�?teri ID',
-    Freight AS 'Kargo �creti'
+    OrderID AS 'Sipariş ID',
+    CustomerID AS 'Müşteri ID',
+    Freight AS 'Kargo Ücreti'
 FROM Orders
 WHERE
     Freight BETWEEN 50 AND 100
 ORDER BY Freight ASC;
 
--- Ka� adet m�?terim var
-SELECT COUNT(CustomerID) as 'M�?teri Say?s?' FROM Customers;
+-- Kaç adet müşterim var
+SELECT COUNT(CustomerID) as 'Müşteri Sayısı' FROM Customers;
 
--- Hangi �lkelere ihracat yap?yorum
-SELECT DISTINCT(Country) AS '?hracat Yap?lan �lkeler' FROM Customers;
+-- Hangi ülkelere ihracat yapıyorum
+SELECT DISTINCT(Country) AS 'İhracat Yapılan Ülkeler' FROM Customers;
 
--- ?lk 5 m�?teriyi listeleyelim
-SELECT TOP 5 CompanyName AS 'Firma Ad?', ContactName AS '?leti?im Ad?' FROM Customers;
+-- İlk 5 müşteriyi listeleyelim
+SELECT TOP 5 CompanyName AS 'Firma Adı', ContactName AS 'İletişim Adı' FROM Customers;
 
--- M�?terilerin �dedi?i toplam kargo �creti
-SELECT SUM(Freight) as 'Toplam Kargo �creti' FROM Orders;
+-- Müşterilerin ödediği toplam kargo ücreti
+SELECT SUM(Freight) as 'Toplam Kargo Ücreti' FROM Orders;
 
--- �r�nlerimin ortalama fiyat? nedir
-SELECT AVG(UnitPrice) as 'Ortalama �r�n Fiyat?' FROM Products;
+-- Ürünlerimin ortalama fiyatı nedir
+SELECT AVG(UnitPrice) as 'Ortalama Ürün Fiyatı' FROM Products;
 
--- En pahal? kargo �cretim ne kadard?r
-SELECT MAX(Freight) as 'En Y�ksek Kargo �creti' FROM Orders;
+-- En pahalı kargo ücretim ne kadardır
+SELECT MAX(Freight) as 'En Yüksek Kargo Ücreti' FROM Orders;
 
--- En ucuz �r�n fiyat? ne kadard?r
-SELECT MIN(UnitPrice) as 'En D�?�k �r�n Fiyat?' FROM Products;
+-- En ucuz ürün fiyatı ne kadardır
+SELECT MIN(UnitPrice) as 'En Düşük Ürün Fiyatı' FROM Products;
 
--- ?u anki tarihi ve saati al
-SELECT GETDATE() AS '?u Anki Tarih ve Saat';
+-- Şu anki tarihi ve saati al
+SELECT GETDATE() AS 'Şu Anki Tarih ve Saat';
 
--- 2010-03-25 tarihinin y?l bilgisini geri d�nd�relim
-SELECT YEAR('2010-03-25') AS 'Y?l';
+-- 2010-03-25 tarihinin yıl bilgisini geri döndürelim
+SELECT YEAR('2010-03-25') AS 'Yıl';
